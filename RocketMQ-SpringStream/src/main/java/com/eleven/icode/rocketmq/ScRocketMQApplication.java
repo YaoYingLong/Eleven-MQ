792606e513f0b6ae3cffeb01881574679068fd01
Y31201308 <906271196@qq.com>
2021-11-29 23:52:25 +0800

    feat：补充RocketMQ SpringBoot用例

---
+++ RocketMQ-SpringStream/src/main/java/com/eleven/icode/rocketmq/ScRocketMQApplication.java
@@ -0,0 +1,16 @@
+package com.eleven.icode.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
+
+@EnableBinding({Source.class, Sink.class})
+@SpringBootApplication
+public class ScRocketMQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScRocketMQApplication.class,args);
+    }
+}
